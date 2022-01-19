88ecf4d0c087e80aa4b10efc9cc98ea746821dd5
Alone0316 <rutujamandlik06@gmail.com>
2022-01-19 06:35:25 +0000

    mido: Drop Dirac and Doze

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -24,10 +24,8 @@ import android.util.Log;
 import android.provider.Settings;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.preferences.FileUtils;
 import org.lineageos.settings.soundcontrol.SoundControlSettings;
-import org.lineageos.settings.torch.TorchSettings;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -36,18 +34,8 @@ public class BootCompletedReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(final Context context, Intent intent) {
-        if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
-            if (DEBUG) Log.d(TAG, "Starting Doze service");
-            DozeUtils.startService(context);
-        }
         new DiracUtils(context).onBootCompleted();
 
-        FileUtils.setValue(TorchSettings.TORCH_1_BRIGHTNESS_PATH,
-                Settings.Secure.getInt(context.getContentResolver(),
-                        TorchSettings.KEY_WHITE_TORCH_BRIGHTNESS, 100));
-        FileUtils.setValue(TorchSettings.TORCH_2_BRIGHTNESS_PATH,
-                Settings.Secure.getInt(context.getContentResolver(),
-                        TorchSettings.KEY_YELLOW_TORCH_BRIGHTNESS, 100));
     int gain = Settings.Secure.getInt(context.getContentResolver(),
                 SoundControlSettings.PREF_HEADPHONE_GAIN, 4);
         FileUtils.setValue(SoundControlSettings.HEADPHONE_GAIN_PATH, gain + " " + gain);
