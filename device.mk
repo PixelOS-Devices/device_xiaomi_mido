7e8de831053dfec36a8be3ff59af7725d72279e3
Alone0316 <rutujamandlik06@gmail.com>
2022-04-27 11:32:54 +0000

    mido: Make drm

--- device.mk
+++ device.mk
@@ -196,7 +196,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl:64 \
     android.hardware.drm@1.0-service-lazy \
+    android.hardware.drm@1.1 \
+    android.hardware.drm@1.1.vendor \
+    android.hardware.drm@1.2 \
+    android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3 \
+    android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.3-service.clearkey
 
 # DuckDuckGo
