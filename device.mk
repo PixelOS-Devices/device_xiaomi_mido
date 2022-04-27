ce2708976085188f3c8ed4f127e97b4ee9e6bba4
Alone0316 <rutujamandlik06@gmail.com>
2022-04-27 11:28:45 +0000

    mido: Build multiple power libs

--- device.mk
+++ device.mk
@@ -342,6 +342,10 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
+    android.hardware.power@1.1 \
+    android.hardware.power@1.1.vendor \
+    android.hardware.power@1.2 \
+    android.hardware.power@1.2.vendor \
     android.hardware.power.stats@1.0-service.mock \
     vendor.qti.hardware.perf@2.0.vendor \
     vendor.qti.hardware.perf@2.1.vendor
