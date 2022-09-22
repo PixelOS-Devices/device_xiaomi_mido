92b8fc176b66d349f5d37e781fc1a4aed312668b
Alone0316 <rutujamandlik06@gmail.com>
2022-09-22 12:23:47 +0000

    Revert "mido: Ship DuckDuckGo"
    This reverts commit 87bbe45b3390d2f703afc58867656709d14029da.

--- device.mk
+++ device.mk
@@ -204,10 +204,6 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.3.vendor \
     android.hardware.drm@1.4-service.clearkey
 
-# DuckDuckGo
-PRODUCT_PACKAGES += \
-    DuckDuckGo
-
 # Dex
 PRODUCT_DEX_PREOPT_DEFAULT_COMPILER_FILTER := verify
 
