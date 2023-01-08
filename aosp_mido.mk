95991f7a1d7e06d95bd3eb1787f23459c90feffb
Alone0316 <rutujamandlik06@gmail.com>
2023-01-08 08:53:25 +0000

    mido: Inherit common_full_phone.mk

--- aosp_mido.mk
+++ aosp_mido.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/mido/device.mk)
 
 # Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := mido
