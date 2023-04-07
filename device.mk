1ab0728c856a443732814f7b6c749e4792b9f938
Alone0316 <rutujamandlik06@gmail.com>
2023-04-07 18:02:58 +0000

    mido: Build GCamGOPrebuilt

--- device.mk
+++ device.mk
@@ -164,7 +164,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.6:64 \
     vendor.qti.hardware.camera.device@1.0 \
     camera.msm8953 \
-    libcamshim
+    libcamshim \
+    GCamGOPrebuilt
 
 # Consumerir
 PRODUCT_PACKAGES += \
